3bd2bd10acee4becde0490f5e91f3d48805eb167
IlyaKaplan2002 <kaplan11092002@gmail.com>
2022-05-28 10:55:20 +0300

    zvit added

--- index.R
+++ index.R
@@ -5,14 +5,14 @@ cor.test(Population, Urban.Population)
 cor.test(Population, World.Population)
 cor.test(Urban.Population, World.Population)
 
+# cor(populationModel$model$Population, populationModel$fitted.values)
+# cor(urbanPopulationModel$model$Urban.Population, urbanPopulationModel$fitted.values)
+# cor(worldPopulationModel$model$World.Population, worldPopulationModel$fitted.values)
+
+
 populationModel <- lm(Population ~ Urban.Population + World.Population)
-cor(populationModel$model$Population, populationModel$fitted.values)
-
 urbanPopulationModel <- lm(Urban.Population ~ Population + World.Population)
-cor(urbanPopulationModel$model$Urban.Population, urbanPopulationModel$fitted.values)
-
 worldPopulationModel <- lm(World.Population ~ Urban.Population + Population)
-cor(worldPopulationModel$model$World.Population, worldPopulationModel$fitted.values)
 
 cor.test(populationModel$model$Population, populationModel$fitted.values)
 cor.test(urbanPopulationModel$model$Urban.Population, urbanPopulationModel$fitted.values)
